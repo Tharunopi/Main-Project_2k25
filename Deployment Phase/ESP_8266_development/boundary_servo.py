a3c965dba0cb4983800a5f8e76483c8a65d043c9
Tharun Atithya <tharunaadhi6@gmail.com>
2025-02-23 09:33:19 +0530

    checking servo range

---
+++ Deployment Phase/ESP_8266_development/boundary_servo.py
@@ -0,0 +1,22 @@
+import time, serial
+
+esp = serial.Serial("COM24", 9600, timeout=1)
+time.sleep(3)  # Wait 3 seconds for ESP to initialize
+esp.flush()
+
+positions = [
+    (0, 0),    # Top-left
+    (180, 0),  # Top-right
+    (0, 180),  # Bottom-left
+    (180, 180),# Bottom-right
+    (90, 90)   # Center
+]
+for i in positions:
+    ang1, ang2 = i
+    command = f"{ang1} {ang2}\n"
+    esp.write(command.encode())
+    time.sleep(0.4)
+    response = esp.readline().decode(errors='ignore').strip()
+    print("ESP Response:", response)
+
+esp.close()
--- Deployment Phase/check.py
+++ Deployment Phase/check.py
@@ -1,4 +1,8 @@
-import numpy as np
-import torch
+from ultralytics import YOLO
+import cv2
 
-print("kk")
+model = YOLO('../YOLO_weights/yolo11n.pt')
+
+results = model("D:/aqeel.jpg", show=True)
+
+cv2.waitKey(0)
