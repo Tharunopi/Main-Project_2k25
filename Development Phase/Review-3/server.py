1a982009abfc205c6051cb322e287f89cd9fa87b
Tharun Atithya <tharunaadhi6@gmail.com>
2025-03-30 20:04:41 +0530

    Using FLASK to send recieve data

--- Development Phase/Review-3/server.py
+++ Development Phase/Review-3/server.py
@@ -1,12 +1,20 @@
 from flask import Flask, request, jsonify
-
+import streamlit as st
 app = Flask(__name__)
 
+li = []
 @app.route('/receive_data', methods=['POST'])
 def receive_data():
     data = request.json
     print("📥 Received Data:", data)
+    li.append(data["distance_history"])
     return jsonify({"status": "success", "message": "Data received!"})
+
+@app.route('/get_all_data', methods=['GET'])
+def get_all_data():
+    return jsonify({"status": "success", "data": li})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
+
