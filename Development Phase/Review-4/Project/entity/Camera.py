bdd0cf82cd7eeeed4dde244fd6d52f7640eff340
Tharun Atithya <tharunaadhi6@gmail.com>
2025-04-17 11:50:57 +0530

    working with SQL Server

--- Development Phase/Review-4/Project/entity/Camera.py
+++ Development Phase/Review-4/Project/entity/Camera.py
@@ -2,7 +2,7 @@ import cv2
 
 class Camera:
     def __init__(self, originalWidth:int, originalHeight:int, cameraOption=0):
-        self.cap = cv2.VideoCapture(cameraOption)
+        self.cap = cv2.VideoCapture(r"C:\Stack overflow\Main-Project_2k25\Development Phase\Review-4\Project\group-of-white-people-is-raising-hands-up-in-the-air-clear-blue-sky-is-on-the--SBV-306427565-preview.mp4")
         self.cap.set(3, originalWidth)
         self.cap.set(4, originalHeight)
 
